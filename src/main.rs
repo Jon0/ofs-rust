88dd6993d9daa3897dec010f5afc2594315b5168
Jon0 <Jono4728@gmail.com>
2017-02-13 01:16:08 +1300

    types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate fuse;
+extern crate nanomsg;
+
+
+struct Map<T> {
+    hash: fn (T) -> u64,
+    cmp: fn (T, T) -> bool,
+    data: Vec<u64>,
+}
+
+
+struct Table<T> {
+    rows: Vec<T>,
+    maps: Vec<Map<T>>,
+}
+
+
 fn main() {
-    println!("ofs");
+    let table: Table<i64>;
 }
