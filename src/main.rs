25aa04e4a1865a2b65428223a1b0fd01bc369af0
Jon0 <Jono4728@gmail.com>
2017-03-07 23:14:02 +1300

    sockets

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,101 @@
 extern crate fuse;
+extern crate libc;
 
-use std::net::{TcpListener, TcpStream};
+use std::mem;
+use libc::*;
 
 
-fn handle_client(stream: TcpStream) {
+pub struct SockAddr4 {
 
 }
 
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:2204").unwrap();
-
-    // accept connections and process them, spawning a new thread for each one
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                handle_client(stream);
-            }
-            Err(e) => { /* connection failed */ }
+impl SockAddr4 {
+    pub fn bind(&self, fd: i32) -> i32 {
+        let addr = sockaddr_in {
+            sin_family: AF_INET as u16,
+            sin_addr: in_addr { s_addr: 0 },
+            sin_port: 3648,
+            sin_zero: [0; 8]
+        };
+        unsafe {
+            let addr_ptr: *const sockaddr =  mem::transmute(&addr);
+            let addr_size = mem::size_of::<sockaddr_in>() as u32;
+            return bind(fd, addr_ptr, addr_size);
         }
+    }
+}
+
+
+/*
+ * readable stream
+ */
+pub struct SockStream {
+    fd: i32
+}
+
+
+impl SockStream {
+    pub fn read<T>(&self, mut obj: &mut T) {
+        unsafe {
+            let obj_ptr: *mut c_void = mem::transmute(&mut obj);
+            let obj_size = mem::size_of::<T>() as usize;
+            let read_size = read(self.fd, obj_ptr, obj_size);
+            if read_size < obj_size as isize {
+                println!("size mismatch");
+            }
+        }
+    }
+}
+
+
+
+pub struct SockAcceptor {
+    fd: i32,
+}
+
+
+impl SockAcceptor {
+    pub fn open() -> SockAcceptor {
+        let addr = SockAddr4 {};
+        unsafe {
+            let fd = socket(AF_INET, SOCK_STREAM, 0);
+            let err = addr.bind(fd);
+            if err < 0 {
+                close(fd);
+            }
+            else {
+                listen(fd, 5);
+            }
+            return SockAcceptor { fd: fd };
+        }
+    }
+
+
+    pub fn accept(&self) -> SockStream {
+        let mut addr = sockaddr_in {
+            sin_family: 0,
+            sin_addr: in_addr { s_addr: 0 },
+            sin_port: 0,
+            sin_zero: [0; 8]
+        };
+        unsafe {
+            let addr_ptr: *mut sockaddr = mem::transmute(&mut addr);
+            let mut addr_size = mem::size_of::<sockaddr_in>() as u32;
+            let addr_size_ptr = &mut addr_size;
+            let newfd = accept(self.fd, addr_ptr, addr_size_ptr);
+            return SockStream { fd: newfd };
+        }
+    }
+}
+
+
+fn main() {
+    let acceptor = SockAcceptor::open();
+    loop {
+        let socket = acceptor.accept();
+        println!("socket connected");
+        let mut input: u32 = 0;
+        socket.read(&mut input);
     }
 }
