acc655bf3a6c741f565814699d5034ac40d3fdfc
Jon0 <Jono4728@gmail.com>
2017-04-05 21:31:25 +1200

    sockets

--- src/main.rs
+++ src/main.rs
@@ -2,104 +2,10 @@ extern crate fuse;
 extern crate libc;
 
 pub mod message;
+pub mod socket;
 
-use std::mem;
-use libc::*;
 use message::*;
-
-
-pub trait Bindable {
-    fn bind(&self, fd: i32) -> i32;
-}
-
-
-pub struct SockAddr4 {
-    port: u16,
-}
-
-
-impl Bindable for SockAddr4 {
-    fn bind(&self, fd: i32) -> i32 {
-        let addr = sockaddr_in {
-            sin_family: AF_INET as u16,
-            sin_addr: in_addr { s_addr: 0 },
-            sin_port: self.port.to_be(),
-            sin_zero: [0; 8]
-        };
-        unsafe {
-            let addr_ptr: *const sockaddr =  mem::transmute(&addr);
-            let addr_size = mem::size_of::<sockaddr_in>() as u32;
-            return bind(fd, addr_ptr, addr_size);
-        }
-    }
-}
-
-
-/*
- * readable stream
- * add generic address type
- */
-pub struct SockStream {
-    fd: i32
-}
-
-
-impl SockStream {
-    pub fn read<T>(&self, mut obj: &mut T) -> Result<usize, i32> {
-        unsafe {
-            let obj_ptr: *mut c_void = mem::transmute(&mut obj);
-            let obj_size = mem::size_of::<T>() as usize;
-            let read_size = read(self.fd, obj_ptr, obj_size);
-            if read_size < 0 {
-                return Err(read_size as i32);
-            }
-            else {
-                return Ok(read_size as usize);
-            }
-        }
-    }
-}
-
-
-
-pub struct SockAcceptor {
-    fd: i32,
-}
-
-
-impl SockAcceptor {
-    pub fn open<T: Bindable>(addr: &T) -> Result<SockAcceptor, i32> {
-        unsafe {
-            let fd = socket(AF_INET, SOCK_STREAM, 0);
-            let err = addr.bind(fd);
-            if err < 0 {
-                close(fd);
-                return Err(err);
-            }
-            else {
-                listen(fd, 5);
-                return Ok(SockAcceptor { fd: fd });
-            }
-        }
-    }
-
-
-    pub fn accept(&self) -> SockStream {
-        let mut addr = sockaddr_in {
-            sin_family: 0,
-            sin_addr: in_addr { s_addr: 0 },
-            sin_port: 0,
-            sin_zero: [0; 8]
-        };
-        unsafe {
-            let addr_ptr: *mut sockaddr = mem::transmute(&mut addr);
-            let mut addr_size = mem::size_of::<sockaddr_in>() as u32;
-            let addr_size_ptr = &mut addr_size;
-            let newfd = accept(self.fd, addr_ptr, addr_size_ptr);
-            return SockStream { fd: newfd };
-        }
-    }
-}
+use socket::*;
 
 
 fn read_message(socket: &mut SockStream) -> AnyMessage {
@@ -113,7 +19,7 @@ fn read_message(socket: &mut SockStream) -> AnyMessage {
 
 
 fn main() {
-    let addr = SockAddr4 { port: 1234 };
+    let addr = SockAddr4::create(1234);
     let mut serv = Server::init();
     match SockAcceptor::open(&addr) {
         Ok(acceptor) => loop {
