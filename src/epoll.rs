42e0e101ba6f15d1e58ca731ba44f3ffbc823a71
Jon0 <Jono4728@gmail.com>
2017-04-07 23:48:54 +1200

    poll

--- src/epoll.rs
+++ src/epoll.rs
@@ -13,4 +13,15 @@ impl EventHandler {
             return EventHandler{ epollfd: efd };
         }
     }
+
+
+
+    pub fn poll(&self) {
+        //let mut event = [epoll_event { events: 0, u64: 0 }; 32];
+        let mut event = epoll_event { events: 0, u64: 0 };
+        unsafe {
+            let result = epoll_wait(self.epollfd, &mut event, 1, -1);
+        }
+
+    }
 }
